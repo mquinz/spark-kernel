e2d549b5e73d70892dbc91a77ef4b443ca811683
stevelowenthal <steve@datastax.com>
2015-09-03 20:19:25 -0700

    Add some CQL magic!

---
+++ kernel/src/main/scala/com/ibm/spark/magic/builtin/Cql.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ibm.spark.magic.builtin
+
+import org.apache.spark.SparkConf
+import com.datastax.spark.connector.cql.CassandraConnector
+import scala.collection.JavaConversions._
+
+import java.io.PrintStream
+
+import com.google.common.base.Strings
+import com.ibm.spark.kernel.protocol.v5.MIMEType
+import com.ibm.spark.magic._
+import com.ibm.spark.magic.dependencies.{IncludeSparkContext, IncludeOutputStream}
+import com.ibm.spark.utils.ArgumentParsingSupport
+import com.ibm.spark.magic.builtin.CqlHelper._
+
+
+
+class Cql extends CellMagic with ArgumentParsingSupport
+with IncludeOutputStream with IncludeSparkContext {
+
+  // Lazy because the outputStream is not provided at construction
+  private lazy val printStream = new PrintStream(outputStream)
+
+  override def execute(code: String): CellMagicOutput = {
+    def printHelpAndReturn: CellMagicOutput = {
+      printHelp(printStream, """%%Cql <cql statement>""")
+      CellMagicOutput()
+    }
+
+    Strings.isNullOrEmpty(code) match {
+      case true => printHelpAndReturn
+      case false => CellMagicOutput(MIMEType.TextHtml -> htmlFromCql(code))
+    }
+  }
+
+  def htmlFromCql(code: String) =  {
+//    val connector = CassandraConnector(new SparkConf().set("spark.cassandra.connection.host", "127.0.0.1"))
+    val connector = CassandraConnector(sparkContext.getConf)
+
+    "<table>" + connector.withSessionDo{
+      session => {val rows = session.execute(code)
+        val cols = rows.getColumnDefinitions
+        "<tr>" + (for (col <- cols) yield "<th>" + col.getName + "</th>" ).mkString + "<tr>" +
+          (for (row <- rows)
+            yield "<tr>"
+              + (for (col <- cols) yield "<td>" + row.getStringForCol(col)
+              + "</td>"
+              ).mkString + "</tr>").mkString
+      }
+    } + "</table>"
+  }
+}
---
+++ kernel/src/main/scala/com/ibm/spark/magic/builtin/CqlHelper.scala
@@ -0,0 +1,60 @@
+package com.ibm.spark.magic.builtin
+
+import java.text.SimpleDateFormat
+import com.datastax.driver.core.DataType.Name._
+import scala.collection.JavaConverters._
+import com.datastax.driver.core.{DataType, UDTValue, GettableByNameData}
+
+/**
+ * DataStax Academy Sample Application
+ *
+ * Copyright 2015 DataStax
+ *
+ */
+object CqlHelper {
+  val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+
+  implicit class EscapableString(s: String) {
+    def escape = xml.Utility.escape(s)
+  }
+
+  implicit class GettableAsString[RowOrUDT <: GettableByNameData](row: RowOrUDT) {
+    def getAsString(colName: String, colType: DataType): String = {
+      colType.getName match {
+        case ASCII => row.getString(colName)
+        case BIGINT => row.getLong(colName)
+        case BLOB => row.getString(colName)
+        case BOOLEAN => row.getBool(colName)
+        case COUNTER => row.getLong(colName)
+        case CUSTOM => row.getString(colName)
+        case DECIMAL => row.getDecimal(colName)
+        case DOUBLE => row.getDouble(colName)
+        case FLOAT => row.getFloat(colName)
+        case INET => row.getInet(colName)
+        case INT => row.getInt(colName)
+        case TEXT => row.getString(colName)
+        case TIMESTAMP => df.format(row.getDate(colName))
+        case TIMEUUID => row.getUUID(colName)
+        case TUPLE => row.getTupleValue(colName)
+        case UUID => row.getUUID(colName)
+        case VARCHAR => row.getString(colName)
+        case VARINT => row.getVarint(colName)
+        case LIST => row.getList(colName, colType.getTypeArguments.get(0).asJavaClass)
+        case MAP => row.getMap(colName, colType.getTypeArguments.get(0).asJavaClass, colType.getTypeArguments.get(1).asJavaClass)
+        case SET => row.getSet(colName, colType.getTypeArguments.get(0).asJavaClass)
+        case UDT => row.getUDTValue(colName).getType.getFieldNames
+      }
+    }.toString
+  }
+
+  implicit class RowHelper(row: com.datastax.driver.core.Row) {
+    def getStringForCol(colDef: com.datastax.driver.core.ColumnDefinitions.Definition): String = {
+      val colName = colDef.getName
+
+      if (row.isNull(colName))
+        "<null>"
+      else
+          row.getAsString(colName, colDef.getType)
+    }.escape
+  }
+}
