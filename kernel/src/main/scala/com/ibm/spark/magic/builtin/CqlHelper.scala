0bbf21b48aebc45fe22d864dc7e9b7d8a04e5eb0
stevelowenthal <steve@datastax.com>
2016-02-28 10:54:50 -0800

    Make null the same for cql and sql

--- kernel/src/main/scala/com/ibm/spark/magic/builtin/CqlHelper.scala
+++ kernel/src/main/scala/com/ibm/spark/magic/builtin/CqlHelper.scala
@@ -53,7 +53,7 @@ object CqlHelper {
       val colName = colDef.getName
 
       if (row.isNull(colName))
-        "<null>"
+        "null"
       else
           row.getAsString(colName, colDef.getType)
     }.escape
